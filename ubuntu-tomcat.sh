8f0edcd03fa5ec42b770da5b6625ee88995097e5
Srushti Meshram <srushtibackupid@gmail.com>
2026-04-19 20:29:58 +0530

    Update ubuntu-tomcat.sh

--- ubuntu-tomcat.sh
+++ ubuntu-tomcat.sh
@@ -5,7 +5,7 @@ wget https://dlcdn.apache.org/tomcat/tomcat-9/v11.0.21/bin/apache-tomcat-11.0.21
 tar -zxvf apache-tomcat-11.0.21.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.21/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.21/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.21/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="srushti123" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.21/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.21/conf/tomcat-users.xml
 sed -i '56d' apache-tomcat-11.0.21/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-11.0.21/webapps/manager/META-INF/context.xml
